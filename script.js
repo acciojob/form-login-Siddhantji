e9427b39889b4527c637d58c319ad25ca870a902
Siddhant Teotia <91278902+Siddhantji@users.noreply.github.com>
2024-04-11 19:48:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 function getFormvalue() {
     //Write your code here
 	let form = document.getElementById("form1");
-	form.addEvenetListener("submit",function(){
-		const f = from.elements["fname"].value;
-		const l = from.elements["lname"].value;
+	form.addEventListener("submit",function(){
+		const f = form.elements["fname"].value;
+		const l = form.elements["lname"].value;
 
 		const alertm = f+" "+l;
 		alert(alertm);
